75fe3a5ec12b6f76158cd9f040efbe797983efd8
Zack Gomez <zack.gomez@gmail.com>
2017-03-21 00:18:23 -0700

    Add board class

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
+/* @flow */
 import fs from 'fs';
 
-const test_board = [
-  '-', '-', 'b', '-',
-  '-', 'X', '-', '-',
-  '-', '-', 'X', '-',
-  '-', 'a', '-', '-',
-];
+type Cell = 'Empty' | 'Blocking' | 'Source' | 'Target';
 
 class Board {
+  width: number;
+  height: number;
+  board: Array<Cell>;
+
+  constructor() {
+    this.height = 4;
+    this.width = 4;
+    this.board = [
+      '-', '-', 'b', '-',
+      '-', 'X', '-', '-',
+      '-', '-', 'X', '-',
+      '-', 'a', '-', '-',
+    ].map(x => {return {'-': 'Empty', 'X': 'Blocking', 'a': 'Source', 'b': 'Target'}[x]});
+  }
+
+  get(x: number, y: number): Cell {
+    return this.board[x + y * this.width];
+  }
+}
+
+
+const board = new Board();
+
+for (let y = 0; y < board.height; y++) {
+  for (let x = 0; x < board.width; x++) {
+    console.log(board.get(x, y));
+  }
 }
