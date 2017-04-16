1760d9d71fbd07b456c9d960454ef6aa4abdaf2d
Zack Gomez <zack.gomez@gmail.com>
2017-04-15 20:52:06 -0700

    Drag support

--- entry.js
+++ entry.js
@@ -26,6 +26,7 @@ for (let y = 0; y <= HEIGHT; y += 50) {
 }
 stage.addChild(grid);
 
+
 let figure = new PIXI.Graphics();
 figure.beginFill(0x9966FF);
 figure.drawCircle(0, 0, 25);
@@ -35,6 +36,76 @@ figure.drawCircle(0, 0, 25);
 figure.endFill();
 figure.x = 75;
 figure.y = 75;
+figure.interactive = true;
 stage.addChild(figure);
 
-renderer.render(stage);
+let mouseInfo = new PIXI.Text('Derp', new PIXI.TextStyle({
+  fontSize: 12,
+  stroke: '#00FF00',
+  fill: '#00FF00',
+}));
+stage.addChild(mouseInfo);
+
+const interactionManager = renderer.plugins.interaction;
+
+let dragData = {
+  startCellX: null,
+  startCellY: null,
+  startX: null,
+  startY: null,
+  entityStartX: null,
+  entityStaryY: null,
+  entity: null,
+};
+
+function render() {
+  const mousePosition = interactionManager.mouse.global;
+
+  mouseInfo.text = `Mouse x: ${mousePosition.x} y: ${mousePosition.y}`;
+
+  renderer.render(stage);
+}
+
+interactionManager.on('pointermove', e => {
+  //console.log('move', e.data.getLocalPosition(stage));
+  if (dragData.entity) {
+    dragData.entity.x = dragData.entityStartX + (e.data.global.x - dragData.startX);
+    dragData.entity.y = dragData.entityStartY + (e.data.global.y - dragData.startY);
+  }
+  render();
+});
+interactionManager.on('pointerdown', e => {
+  if (!e.currentTarget) {
+    return;
+  }
+  if (dragData.entity) return;
+  dragData.entity = e.target;
+  dragData.entityStartX = e.target.x;
+  dragData.entityStartY = e.target.y;
+  dragData.startX = e.data.global.x;
+  dragData.startY = e.data.global.y;
+  dragData.startCellX = Math.floor(dragData.startX / 50);
+  dragData.startCellY = Math.floor(dragData.startY / 50);
+  console.log('down', e.data.getLocalPosition(stage));
+  render();
+});
+interactionManager.on('pointerup', e => {
+  const position = e.data.getLocalPosition(stage);
+  console.log('up', position);
+  if (dragData.entity) {
+    console.log(dragData);
+    const targetCellX = Math.floor(position.x / 50);
+    const targetCellY = Math.floor(position.y / 50);
+    dragData.entity.x = targetCellX * 50 + 25;
+    dragData.entity.y = targetCellY * 50 + 25;
+    dragData.entity = null;
+  }
+  render();
+});
+interactionManager.on('rightdown', e => {
+  e.stopPropagation();
+  console.log('rightdown', e.data.getLocalPosition(stage));
+  render();
+});
+
+render();
