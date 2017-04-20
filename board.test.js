ab8a384cdb7127f093bb3fee110f5eb8bd90fe6f
rtagher <austinmoehle100@gmail.com>
2017-04-20 00:57:39 -0700

    Updated test cases for LoS and ray casting

--- board.test.js
+++ board.test.js
@@ -28,6 +28,10 @@ test('blocked edges', () => {
     [5, 1, 0, 1],
     [4, 2, 0, 1],
     [0, 1, 4, 2],
+    [0, 0, 1, 1],
+    [1, 1, 0, 0],
+    [0, 0, 5, 1],
+    [5, 1, 0, 0],        
   ];
   let expectedResults = [
     false, 
@@ -44,6 +48,10 @@ test('blocked edges', () => {
     false,
     false,
     true,
+    true,
+    true,
+    false,
+    false,
   ];
   let results = [];
   for (let i = 0; i < checks.length; i++) {
@@ -56,47 +64,3 @@ test('blocked edges', () => {
 
   expect(results.every((s, i) => s.hasLineOfSight === expectedResults[i])).toBe(true);
 });
-
-/*test('board edges', () => {
-  let board = new Board(2, 2, ['Empty', 'Empty', 'Empty', 'Empty']);
-
-  expect(board.getEdge(0, 0, 'Down')).toBe('Blocked');
-  expect(board.getEdge(0, 1, 'Down')).toBe('Blocked');
-  expect(board.getEdge(2, 0, 'Down')).toBe('Blocked');
-  expect(board.getEdge(2, 1, 'Down')).toBe('Blocked');
-
-  expect(board.getEdge(0, 0, 'Right')).toBe('Blocked');
-  expect(board.getEdge(1, 0, 'Right')).toBe('Blocked');
-  expect(board.getEdge(0, 2, 'Right')).toBe('Blocked');
-  expect(board.getEdge(1, 2, 'Right')).toBe('Blocked');
-});
-
-test('disconnected by diagonal blockers', () => {
-  let board = new Board(2, 2, ['Empty', 'Blocking', 'Blocking', 'Empty']);
-
-  expect(board.areCellsConnected(0, 0, 1, 1)).toBe(false);
-  expect(board.areCellsConnected(1, 1, 0, 0)).toBe(false);
-});
-
-test('connected with one blocked edge', () => {
-  let board = new Board(2, 2, ['Empty', 'Empty', 'Blocking', 'Empty']);
-
-  // diagonal
-  expect(board.areCellsConnected(0, 0, 1, 1)).toBe(true);
-  expect(board.areCellsConnected(1, 1, 0, 0)).toBe(true);
-
-  // horizontal
-  expect(board.areCellsConnected(0, 0, 1, 0)).toBe(true);
-  expect(board.areCellsConnected(1, 0, 0, 0)).toBe(true);
-
-  // vertical
-  expect(board.areCellsConnected(1, 0, 1, 1)).toBe(true);
-  expect(board.areCellsConnected(1, 1, 1, 0)).toBe(true);
-});
-
-test('disconnected by horizontal wall', () => {
-  let board = new Board(2, 2, ['Empty', 'Empty', 'Blocking', 'Blocking']);
-  expect(board.areCellsConnected(0, 0, 1, 1)).toBe(false);
-  expect(board.areCellsConnected(1, 1, 0, 0)).toBe(false);
-});
-*/
