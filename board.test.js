467bb11bba1a7fef8cbb2a06405bf03db932fee4
rtagher <austinmoehle100@gmail.com>
2017-04-20 00:36:45 -0700

    Added edges to board and updated line of sight rules

--- board.test.js
+++ board.test.js
@@ -1,18 +1,63 @@
-import Board from './board';
+import Board, {boardFromRows} from './board';
+import fs from 'mz/fs';
 
 test('blocked edges', () => {
-  let board = new Board(2, 2, ['Empty', 'Empty', 'Blocking', 'Empty']);
+  const contents = fs.readFileSync('derpy.map', {encoding: 'utf8'});
+  console.log('Read Map:');
+  console.log(contents);
+  let rows = contents.split('\n');
+  rows = rows.filter(s => s.length > 0);
 
-  expect(board.getEdge(0, 1, 'Down')).toBe('Blocked');
-  expect(board.getEdge(0, 1, 'Right')).toBe('Blocked');
+  rows = rows.map(row => row.split(''));
+  const board = boardFromRows(rows);
+  console.log('Original board:');
+  board.printBoard();
+  console.log('Testing line of sight...');
+  let checks = [
+    [5, 1, 5, 3],
+    [5, 3, 5, 1],
+    [5, 3, 0, 2],
+    [0, 2, 5, 3],
+    [1, 1, 2, 0],
+    [2, 0, 1, 1],
+    [4, 0, 5, 0],
+    [5, 0, 4, 0],
+    [4, 0, 5, 1],
+    [5, 1, 4, 0],
+    [0, 1, 5, 1],
+    [5, 1, 0, 1],
+    [4, 2, 0, 1],
+    [0, 1, 4, 2],
+  ];
+  let expectedResults = [
+    false, 
+    true, 
+    true, 
+    true, 
+    true, 
+    true, 
+    true,
+    true,
+    false,
+    false,
+    false,
+    false,
+    false,
+    true,
+  ];
+  let results = [];
+  for (let i = 0; i < checks.length; i++) {
+    let check = checks[i];
+    let result = board.checkLineOfSight(check[0], check[1], check[2], check[3]);
+    board.printLineOfSightResult(check[0], check[1], check[2], check[3], result);
+    console.log(result);
+    results.push(result);
+  }
 
-  expect(board.getEdge(1, 1, 'Down')).toBe('Blocked');
-  expect(board.getEdge(1, 1, 'Right')).toBe('Clear');
-
-  expect(board.getEdge(0, 2, 'Right')).toBe('Blocked');
+  expect(results.every((s, i) => s.hasLineOfSight === expectedResults[i])).toBe(true);
 });
 
-test('board edges', () => {
+/*test('board edges', () => {
   let board = new Board(2, 2, ['Empty', 'Empty', 'Empty', 'Empty']);
 
   expect(board.getEdge(0, 0, 'Down')).toBe('Blocked');
@@ -54,3 +99,4 @@ test('disconnected by horizontal wall', () => {
   expect(board.areCellsConnected(0, 0, 1, 1)).toBe(false);
   expect(board.areCellsConnected(1, 1, 0, 0)).toBe(false);
 });
+*/
