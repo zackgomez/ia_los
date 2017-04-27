9937e6ca3eb50a49348d42a38a5e68bcd5527476
Zack Gomez <zacharyg@caltech.edu>
2017-04-27 02:12:45 -0700

    separate piece and figure data models.  rebuild entire graph each frame for convenience

--- entry.js
+++ entry.js
@@ -1,9 +1,13 @@
+/* @flow weak */
+
 'use strict';
 
-import raycast from './RayCast.js';
 import 'pixi.js';
 import Board, {emptyBoard, boardFromMapFile, cellToPoint} from './board.js';
+import type {Point} from './board.js';
 import 'isomorphic-fetch';
+import _ from 'lodash';
+import nullthrows from 'nullthrows';
 
 const VIEWPORT_WIDTH = 640;
 const VIEWPORT_HEIGHT = 480;
@@ -12,9 +16,9 @@ let renderer = new PIXI.autoDetectRenderer(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
 renderer.backgroundColor = 0x777777;
 //renderer.autoResize = true;
 
-document.body.appendChild(renderer.view);
-
-let stage = new PIXI.Container();
+if (document.body) {
+  document.body.appendChild(renderer.view);
+}
 
 const interactionManager = renderer.plugins.interaction;
 
@@ -26,6 +30,7 @@ let dragData = {
   entityStartX: null,
   entityStaryY: null,
   entity: null,
+  piece: null,
 };
 
 const SCALE = 50;
@@ -73,7 +78,7 @@ let nextFigureID = 1;
 function makeFigureID(): string {
   return 'id' + nextFigureID++;
 }
-function makeFigure(id: string, type: string, color: any, x: number, y: number) {
+function makeFigureView(id: string, type: string, color: any, x: number, y: number) {
   let figure = new PIXI.Graphics();
   figure.beginFill(color);
   figure.drawCircle(0, 0, SCALE / 2);
@@ -90,97 +95,151 @@ function makeFigure(id: string, type: string, color: any, x: number, y: number) 
 
   return figure;
 }
+function makeFigure(id: string, type: string, color: number, x: number, y: number) {
+  return {
+    id,
+    type,
+    color,
+    x,
+    y,
+  };
+}
 
+let selectedFigureID: ?string = null;
+type Piece = {
+  id: string;
+  type: string;
+  color: number;
+  cellX: number;
+  cellY: number;
+  dragPosition: ?Point;
+}
 let figures = {};
 
-function makeFigureLayer() {
-  let layer = new PIXI.Container();
-  // source
+function setupFigures() {
   const source = makeFigure(makeFigureID(), 'source', 0x0000FF, 2, 2);
-  layer.addChild(source);
   figures[source.id] = source;
 
   const target = makeFigure(makeFigureID(), 'target', 0xDD1122, 2, 3);
-  layer.addChild(target);
   figures[target.id] = target;
 
   _.times(4, (i) => {
     const fig = makeFigure(makeFigureID(), 'neutral', 0x333333, i, 0);
-    layer.addChild(fig);
     figures[fig.id] = fig;
+  });
+}
+
+function makeFigureLayer() {
+  let layer = new PIXI.Container();
+  _.each(figures, (figure, id) => {
+    const view = makeFigureView(
+      figure.id, figure.type, figure.color, figure.x, figure.y
+    );
+    if (figure.dragPosition) {
+      view.x = figure.dragPosition.x;
+      view.y = figure.dragPosition.y;
+    }
+    layer.addChild(view);
   });
 
   return layer;
 }
 
-let mouseInfo = new PIXI.Text('Derp', new PIXI.TextStyle({
-  fontSize: 12,
-  stroke: '#00FF00',
-  fill: '#00FF00',
-}));
-stage.addChild(mouseInfo);
-
+let stage = null;
 function render() {
   const mousePosition = interactionManager.mouse.global;
 
+  const root = new PIXI.Container();
+
+  const board = globalBoard;
+  if (board) {
+    root.addChild(getGridLayer(board));
+    root.addChild(getEdgeLayer(board));
+    root.addChild(makeFigureLayer());
+    root.addChild(makeLineOfSightLayer(board, figures));
+  }
+
+  let mouseInfo = new PIXI.Text('Derp', new PIXI.TextStyle({
+    fontSize: 12,
+    stroke: '#00FF00',
+    fill: '#00FF00',
+  }));
   mouseInfo.text = `Mouse x: ${mousePosition.x} y: ${mousePosition.y}`;
 
-  renderer.render(stage);
+  root.addChild(mouseInfo);
+  stage = root;
+
+  renderer.render(root);
 }
 
-interactionManager.on('pointermove', e => {
-  //console.log('move', e.data.getLocalPosition(stage));
-  if (dragData.entity) {
-    dragData.entity.x = dragData.entityStartX + (e.data.global.x - dragData.startX);
-    dragData.entity.y = dragData.entityStartY + (e.data.global.y - dragData.startY);
+interactionManager.on('mousemove', e => {
+  if (dragData.piece) {
+    dragData.piece.dragPosition = {
+      x: dragData.entityStartX + (e.data.global.x - dragData.startX),
+      y: dragData.entityStartY + (e.data.global.y - dragData.startY),
+    };
   }
   render();
 });
-interactionManager.on('pointerdown', e => {
+interactionManager.on('mousedown', e => {
   if (!e.currentTarget) {
     return;
   }
   if (dragData.entity) return;
   dragData.entity = e.target;
+  dragData.piece = figures[e.target.id];
   dragData.entityStartX = e.target.x;
   dragData.entityStartY = e.target.y;
   dragData.startX = e.data.global.x;
   dragData.startY = e.data.global.y;
-  dragData.startCellX = Math.floor(dragData.startX / 50);
-  dragData.startCellY = Math.floor(dragData.startY / 50);
+  dragData.startCellX = Math.floor(dragData.startX / SCALE);
+  dragData.startCellY = Math.floor(dragData.startY / SCALE);
   console.log('down', e.data.getLocalPosition(stage));
   render();
 });
-interactionManager.on('pointerup', e => {
+interactionManager.on('mouseup', e => {
   const position = e.data.getLocalPosition(stage);
   console.log('up', position);
   if (dragData.entity) {
     console.log(dragData);
     const targetCellX = Math.floor(position.x / 50);
     const targetCellY = Math.floor(position.y / 50);
-    dragData.entity.x = targetCellX * 50 + 25;
-    dragData.entity.y = targetCellY * 50 + 25;
+    dragData.entity.x = targetCellX * SCALE + SCALE/2;
+    dragData.entity.y = targetCellY * SCALE + SCALE/2;
     dragData.entity = null;
+    dragData.piece.dragPosition = null;
+    dragData.piece.x = targetCellX;
+    dragData.piece.y = targetCellY;
+    dragData.piece = null;
   }
   updateBoardWithFigures(globalBoard, figures);
-  updateLineOfSight(globalBoard, figures, lineOfSightViews);
+  updateLineOfSight(globalBoard, figures);
   render();
 });
-interactionManager.on('rightdown', e => {
-  e.stopPropagation();
-  console.log('rightdown', e.data.getLocalPosition(stage));
-  render();
-});
+
+type ToolEnum = 'pointer';
+type UIState = {
+  currentTool: ToolEnum;
+  availableTools: Array<ToolEnum>;
+};
+
+let uiState: UIState = {
+  currentTool: 'pointer',
+  availableTools: [],
+};
+
+function makeUILayer(state: UIState) {
+  let layer = new PIXI.Container();
+
+  return layer;
+}
 
 render();
 
 let globalBoard = null;
 function setBoard(board) {
   globalBoard = board;
-  stage.addChild(getGridLayer(board));
-  stage.addChild(getEdgeLayer(board));
-  stage.addChild(makeFigureLayer());
-  stage.addChild(makeLineOfSightLayer());
+  setupFigures();
 }
 
 function updateBoardWithFigures(board, figures) {
@@ -196,20 +255,6 @@ function updateBoardWithFigures(board, figures) {
   board.printBoard();
 }
 
-let lineOfSightViews = {
-  layer: null,
-  lines: null,
-};
-
-function makeLineOfSightLayer() {
-  const layer = new PIXI.Container();
-
-  lineOfSightViews.layer = layer;
-  lineOfSightViews.lines = [];
-
-  return layer;
-}
-
 function makeLine(
   x0: number, y0: number,
   x1: number, y1: number,
@@ -221,47 +266,59 @@ function makeLine(
   return line;
 }
 
-function updateLineOfSight(board, figures, lineOfSightViews) {
+let lastSourcePosition = null;
+let lastTargetPosition = null;
+let lastLineOfSightResult = null;
+
+function updateLineOfSight(board: Board, figures) {
   let sourceX, sourceY, targetX, targetY;
   _.each(figures, (fig) => {
     if (fig.type === 'source') {
-      sourceX = Math.floor(fig.x / SCALE);
-      sourceY = Math.floor(fig.y / SCALE);
+      sourceX = Math.floor(fig.x);
+      sourceY = Math.floor(fig.y);
     } else if (fig.type === 'target') {
-      targetX = Math.floor(fig.x / SCALE);
-      targetY = Math.floor(fig.y / SCALE);
+      targetX = Math.floor(fig.x);
+      targetY = Math.floor(fig.y);
     }
   });
+
   console.log(sourceX, sourceY, targetX, targetY);
   let result = board.checkLineOfSight(sourceX, sourceY, targetX, targetY);
   console.log(result);
+  lastLineOfSightResult = result;
+  lastSourcePosition = {x: sourceX, y: sourceY};
+  lastTargetPosition = {x: targetX, y: targetY};
+}
 
-  _.each(lineOfSightViews.lines, (line) => {
-    lineOfSightViews.layer.removeChild(line);
-  });
-  lineOfSightViews.lines = [];
+function makeLineOfSightLayer(board: Board, figures) {
+  const layer = new PIXI.Container();
 
-  if (result.hasLineOfSight) {
-    const sourcePoint = cellToPoint({x: sourceX, y: sourceY}, result.sourceCorner);
-    const targetCellPoint = {x: targetX, y: targetY};
+  let sourceX, sourceY, targetX, targetY;
+
+  const result = lastLineOfSightResult;
+  if (result && result.hasLineOfSight) {
+    const sourcePoint = cellToPoint(
+      nullthrows(lastSourcePosition),
+      result.sourceCorner,
+    );
+    const targetCellPoint = nullthrows(lastTargetPosition);
     const pointA = cellToPoint(targetCellPoint, result.targetCorners[0]);
     const pointB = cellToPoint(targetCellPoint, result.targetCorners[1]);
-    lineOfSightViews.lines.push(
+    layer.addChild(
       makeLine(sourcePoint.x, sourcePoint.y, pointA.x, pointA.y),
     );
-    lineOfSightViews.lines.push(
+    layer.addChild(
       makeLine(sourcePoint.x, sourcePoint.y, pointB.x, pointB.y),
     );
   }
-  _.each(lineOfSightViews.lines, (line) => {
-    lineOfSightViews.layer.addChild(line);
-  });
+  return layer;
 }
 
 const board = emptyBoard(10, 10);
 setBoard(board);
 board.printBoard();
 
+/*
 fetch('/api/board').then(response => {
   if (!response.ok) {
     alert('Unable to fetch board');
@@ -273,3 +330,4 @@ fetch('/api/board').then(response => {
   board.printBoard();
   setBoard(board);
 });
+*/
